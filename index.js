b10b758ff883a0ad5992867e0b1370208c58ba64
Sarwar-Ahmed <sarwarahmed109@gmail.com>
2020-09-29 09:50:48 +0600

    concept almost clear

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const MongoClient = require('mongodb').MongoClient;
+const objectId = require('mongodb').ObjectID;
 
 const uri = "mongodb+srv://organicUser:9ypJWrP5bpvGHZD@cluster0.ltrdg.mongodb.net/organicdb?retryWrites=true&w=majority";
 
@@ -22,12 +24,42 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const productCollection = client.db("organicdb").collection("products");
   // perform actions on the collection object
+    app.get('/products', (req, res) => {
+        productCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents)
+        })
+    })
+
+    app.get(`/product/:id`, (req, res) => {
+        productCollection.find({_id: ObjectID(req.params.id)})
+        .toArray((err, documents) => {
+            res.send(documents[0]);
+        })
+    })
+
     app.post("/addProduct", (req, res) => {
         const product = req.body;
         productCollection.insertOne(product)
         .then(result => {
-            console.log('data added successfully');
-            res.send('success');
+            res.redirect('/');
+        })
+    })
+
+    app.patch('/update/:id', (req, res) => {
+        productCollection.updateOne({_id: ObjectID(req.params.id)},
+        {
+            $set: {price: req.body.price, quantity: req.body.quantity}
+        })
+        .then(result => {
+            res.send(result.modifiedCount > 0);
+        })
+    })
+
+    app.delete('/delete/:id', (req, res) =>{
+        productCollection.deleteOne({_id: ObjectID(req.params.id)})
+        .then( (result) => {
+            res.send(result.deletedCount > 0);
         })
     })
     
